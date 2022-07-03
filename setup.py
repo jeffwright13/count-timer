826cf5c179892435e785a0066ef473d267fe4351
Jeff Wright <jeffrey.wright@wartsila.com>
2022-07-03 18:34:52 -0400

    Bumped to version 0.0.2.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name="counters",
-    version="0.0.1",
+    version="0.0.2",
     author="Jeff Wright",
     author_email="jeff.washcloth@gmail.com",
     license="MIT",
