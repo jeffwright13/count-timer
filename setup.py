e80d702be1d5c6f352cc113d391deb185d221609
Jeff Wright <jeffrey.wright@wartsila.com>
2022-07-03 16:01:22 -0400

    Added setup.py for release process.

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import codecs
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding="utf-8").read()
+
+
+setup(
+    name="counters",
+    version="0.0.1",
+    author="Jeff Wright",
+    author_email="jeff.washcloth@gmail.com",
+    license="MIT",
+    url="https://github.com/jeffwright13/counters",
+    description="A collection of counters of various types for general use",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    py_modules=["pytest_tui"],
+    python_requires=">=3.7",
+    install_requires=[],
+    classifiers=[
+        "Framework :: Python",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Testing",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+    ],
+    keywords="python counters countdown timer",
+    entry_points={},
+)
