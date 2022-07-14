cf40d6ad4cad549a982fc140fbfe2931d3bcf04a
Jeff Wright <jeffrey.wright@wartsila.com>
2022-07-14 00:46:11 -0600

    Demo file that counts down from use input duration.

---
+++ count_timer/demo.py
@@ -0,0 +1,56 @@
+from count_timer import CountTimer
+from blessings import Terminal
+
+
+def main():
+    term = Terminal()
+    term.clear()
+
+    duration = input("Enter countdown timer duration: ")
+    counter = CountTimer(duration=float(duration))
+
+    counter.start()
+    while counter.remaining > 10:
+        print(
+            term.bold
+            + term.green
+            + term.move_x(0)
+            + term.move_up
+            + term.clear_eol
+            + str(round(counter.remaining, 3))
+        )
+
+    while counter.remaining > 5:
+        print(
+            term.bold
+            + term.yellow
+            + term.move_x(0)
+            + term.move_up
+            + term.clear_eol
+            + str(round(counter.remaining, 3))
+        )
+
+    while counter.remaining > 0:
+        print(
+            term.bold
+            + term.red
+            + term.move_x(0)
+            + term.move_up
+            + term.clear_eol
+            + str(round(counter.remaining, 3))
+        )
+
+    print(
+        term.bold
+        + term.magenta
+        + term.move_x(0)
+        + term.move_up
+        + term.clear_eol
+        + "TIME'S UP!"
+    )
+    term.clear()
+    print(term.normal)
+
+
+if __name__ == "__main__":
+    main()
